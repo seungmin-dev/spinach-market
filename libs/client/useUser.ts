d2f79725a9a36c4db2b4b73412e6ce511934a81a
seungmin-dev <seungmin.dev@gmail.com>
2023-03-18 17:21:01 +0900

    useSWR

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,20 +1,17 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 export default function useUser() {
-  const [user, setUser] = useState();
+  const { data, error } = useSWR("/api/users/me");
   const router = useRouter();
-
   useEffect(() => {
-    fetch("/api/users/me")
-      .then((response) => response.json())
-      .then((data) => {
-        if (!data.ok) {
-          return router.push("/enter");
-        }
-        setUser(data.profile);
-      });
-  }, [router]);
+    if (data && !data.ok) {
+      router.replace("/enter");
+    }
+  }, [data, router]);
 
-  return user;
+  //   return router.push("/enter");
+
+  return { user: data?.profile, isLoading: !data && !error };
 }
