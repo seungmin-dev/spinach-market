a7a0b27d1b3ebd2d9f669d0a258d16e163549c55
seungmin-dev <seungmin.dev@gmail.com>
2023-03-26 01:13:37 +0900

    sending messages

--- pages/streams/create.tsx
+++ pages/streams/create.tsx
@@ -17,7 +17,7 @@ interface CreateForm {
 
 interface CreateResponse {
   ok: boolean;
-  live: Stream;
+  stream: Stream;
 }
 const Create: NextPage = () => {
   const router = useRouter();
@@ -30,9 +30,9 @@ const Create: NextPage = () => {
   };
   useEffect(() => {
     if (data && data.ok) {
-      router.push(`/stream/${data.stream.id}`);
+      router.push(`/streams/${data.stream.id}`);
     }
-  }, [data]);
+  }, [data, router]);
   return (
     <Layout canGoBack title="Go Live">
       <form onSubmit={handleSubmit(onValid)} className=" space-y-4 py-10 px-4">
@@ -44,7 +44,7 @@ const Create: NextPage = () => {
           type="text"
         />
         <Input
-          register={register("price", { required: true })}
+          register={register("price", { required: true, valueAsNumber: true })}
           required
           label="Price"
           placeholder="0.00"
--- pages/streams/index.tsx
+++ pages/streams/index.tsx
@@ -2,22 +2,30 @@ import type { NextPage } from "next";
 import Link from "next/link";
 import FloatingButton from "@components/floating-button";
 import Layout from "@components/layout";
+import { Stream } from "@prisma/client";
+import useSWR from "swr";
 
-const Stream: NextPage = () => {
+interface StreamsResponse {
+  ok: boolean;
+  streams: Stream[];
+}
+
+const Streams: NextPage = () => {
+  const { data } = useSWR<StreamsResponse>(`/api/streams`);
   return (
     <Layout title="라이브" hasTabBar>
       <div className="divide-y-2 space-y-4">
-        {[1, 2, 3, 4].map((i) => (
-          <Link legacyBehavior key={i} href={`/live/${i}`}>
+        {data?.streams.map((stream) => (
+          <Link legacyBehavior key={stream.id} href={`/streams/${stream.id}`}>
             <a className="pt-4 block  px-4">
               <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
               <h1 className="text-2xl mt-2 font-bold text-gray-900">
-                Galaxy S50
+                {stream.name}
               </h1>
             </a>
           </Link>
         ))}
-        <FloatingButton href="/live/create">
+        <FloatingButton href="/streams/create">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -37,4 +45,4 @@ const Stream: NextPage = () => {
   );
 };
 
-export default Stream;
+export default Streams;
