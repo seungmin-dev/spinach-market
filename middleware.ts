09759068f1087ce9c224d354be656f9da2a93c70
seungmin-dev <seungmin.dev@gmail.com>
2023-04-03 17:45:22 +0900

    middleware - useUser hook

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,23 @@
-import { NextRequest, NextFetchEvent, userAgent } from "next/server";
+import {
+  NextRequest,
+  NextResponse,
+  NextFetchEvent,
+  userAgent,
+} from "next/server";
+
+// export const config = { matcher: ["/((?!_next|api/auth).*)(.+)"] };
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  console.log("it works! globle middleware");
-  console.log(userAgent(req));
+  if (userAgent(req).isBot) {
+    return new Response("Plz don't be a bot. Be human", { status: 403 });
+  }
+
+  if (req.nextUrl.pathname.startsWith("/api")) {
+    if (!req.url.includes("/enter") && !req.cookies.get("carrotsession")) {
+      NextResponse.redirect(`${req.nextUrl.origin}/enter`);
+    }
+  }
 
   if (req.nextUrl.pathname.startsWith("/chats")) {
-    console.log("chats only middleware");
   }
 }
